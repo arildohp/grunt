332979cbac4299c9fad140b35cdf5aad15208d5f
arildo <arildohp@gmail.com>
2024-09-12 13:58:49 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports =  function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compress'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('olaGrunt', function() {
+        console.log('Ola Grunt')
+    })
+    
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['less', 'sass']);
+}
